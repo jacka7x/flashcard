98789821a0ef71719d175aa57163b835808f85a1
Jack Hammond <jash.hammond@gmail.com>
2022-02-11 00:13:55 +0900

    FC-1 sheduling tests completed

--- src/general/scripts/tests/scheduling.test.ts
+++ src/general/scripts/tests/scheduling.test.ts
@@ -1,20 +1,47 @@
-import { getNewSpacingRight } from "../scheduling";
+import { getNewSpacingRight } from "../scheduling"
 
-test('returns spacing multipled by 1.4', () => {
+describe('scheduling', () => {
+
+  test('returns spacing multipled by 1.4', () => {
     expect(getNewSpacingRight(1000)).toBe(1400)
-})
+  })
 
-test('Floors answer of 1398.6 to 1398', () => {
+  test('Floors answer of 1398.6 to 1398', () => {
     expect(getNewSpacingRight(999)).toBe(1398)
-})
+  })
 
-// repeat these tests (.each? loop?)
-test('Returns 5000 for input of 0 and throws error', () => {
-    expect(getNewSpacingRight(0)).toBe(5000)
-})
+  const originalError = console.error
+  afterEach(() => (console.error = originalError))
 
-test('Returns 5000 for negative input and throws error', () => {
-    expect(getNewSpacingRight(-1)).toBe(5000)
+  describe('Input of 0, return and console.error', () => {
+    let consoleOutput: string[] = []
+    const mockedError = (output: string) => consoleOutput.push(output)
+    beforeEach(() => (console.error = mockedError))
+
+    test('Returns 5000 for input of 0', () => {
+      expect(getNewSpacingRight(0)).toBe(5000)
+    })
+
+    test('Console.error [0] for input of 0', () => {
+      expect(consoleOutput[0]?.toString()).toEqual(
+        "Error: Card spacing is negative or zero [0]")
+    })
+  })
+
+  describe('Input of -1, return and console.error', () => {
+    let consoleOutput: string[] = []
+    const mockedError = (output: string) => consoleOutput.push(output)
+    beforeEach(() => (console.error = mockedError))
+
+    test('Returns 5000 for input of -1', () => {
+      expect(getNewSpacingRight(-1)).toBe(5000)
+    })
+
+    test('Console.error [0] for input of -1', () => {
+      expect(consoleOutput[0]?.toString()).toEqual(
+        "Error: Card spacing is negative or zero [-1]")
+    })
+  })
 })
 
 export {}
